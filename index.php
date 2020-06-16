e635a7522f93d3a3a72ce6757cddbc0558c55311
Joe Suarez <joeenmanuel.suarez@hotmail.com>
2020-06-16 10:49:29 -0400

    Formulario1
    Co-Authored-By: jf-2000 <jf-2000@users.noreply.github.com>

--- index.php
+++ index.php
@@ -13,24 +13,41 @@
 <body>
 
     <div class="container">
-        <form>
-        <div class="form-group">
-                <label for="exampleInputEmail1">Correo Electronico</label>
-                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                <small id="emailHelp" class="form-text text-muted">Puto el que lo lea</small>
-                <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-            </div>
-            <div class="form-group">
-                <label for="exampleInputPassword1">Contraseña</label>
-                <input type="password" class="form-control" id="exampleInputPassword1">
-            </div>
-            <div class="form-group form-check">
-                <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                <label class="form-check-label" for="exampleCheck1">Check me out</label>
-            </div>
-            <button type="submit" class="btn btn-primary">Submit</button>
-            <a class="btn btn-danger" href="index.php" role="button">Regresar</a>
-        </form>
+    <form class="form-horizontal">
+<fieldset>
+
+<!-- Form Name -->
+<legend>Form Name</legend>
+
+<!-- Text input-->
+<div class="form-group">
+  <label class="col-md-4 control-label" for="textName">Name</label>  
+  <div class="col-md-4">
+  <input id="textName" name="textName" type="text" placeholder="Pedro Martínez" class="form-control input-md" required="">
+    
+  </div>
+</div>
+
+<!-- Text input-->
+<div class="form-group">
+  <label class="col-md-4 control-label" for="textem">Contraseña</label>  
+  <div class="col-md-4">
+  <input id="textem" name="textem" type="text" placeholder="ejemplo55@picapollo.net" class="form-control input-md" required="">
+    
+  </div>
+</div>
+
+<!-- Password input-->
+<div class="form-group">
+  <label class="col-md-4 control-label" for="passwordinput">Contraseña</label>
+  <div class="col-md-4">
+    <input id="passwordinput" name="passwordinput" type="password" placeholder="********" class="form-control input-md">
+    <span class="help-block">help</span>
+  </div>
+</div>
+
+</fieldset>
+</form>
     </div>
  
     <!-- Jquery -->
